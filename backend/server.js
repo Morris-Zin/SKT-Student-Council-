25f61967ede3984aa74a8f79eb858cd63e9c1eb1
Aung Phone Khant <aungphonekhant@Aungs-MacBook-Pro.local>
2023-11-06 21:57:01 +0630

    hi

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import path from "path";
-import express from "express.js";
+import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
